7f48983902c0bbabbc43396af17b7e14ea8bb48e
Adithya Reddy Barda <130206558+adithyabarda@users.noreply.github.com>
2025-01-14 17:25:34 +0530

    Update Navbar.jsx

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,22 +1,22 @@
-import {Link} from "react-router-dom"
+import { Link } from "react-router-dom";
+
 const Navbar = () => (
-  <nav className="bg-gray-800 p-4">
+  <nav className="bg-gray-800 p-4 sticky top-0 z-50">
     <div className="container mx-auto flex justify-between items-center">
       <h1 className="text-white text-2xl font-bold">CareerCatalyst</h1>
       <div>
-      <Link
-  to="/login"
-  className="px-4 py-2 dark:bg-violet-500 text-white justify-center rounded-full hover:bg-violet-600 mx-2"
->
-  Login
-</Link>
-<Link
-  to="/signup"
-  className="px-4 py-2 dark:bg-violet-500 text-white justify-center rounded-full hover:bg-violet-600 mx-2"
->
-  Sign Up
-</Link>
-
+        <Link
+          to="/login"
+          className="px-4 py-2 dark:bg-violet-500 text-white justify-center rounded-full hover:bg-violet-600 mx-2"
+        >
+          Login
+        </Link>
+        <Link
+          to="/signup"
+          className="px-4 py-2 dark:bg-violet-500 text-white justify-center rounded-full hover:bg-violet-600 mx-2"
+        >
+          Sign Up
+        </Link>
       </div>
     </div>
   </nav>
